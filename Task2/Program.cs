76b3ef2b0c203d58ffe64f855773d1460e36d0dc
maximandreev <maxands@yandex.ru>
2022-08-09 16:28:47 +0300

    SetPassword created

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,13 @@
-﻿while (true)
+﻿string SetPassword()
+{
+    Console.WriteLine("Задайте пароль:");
+    string userPassword = Console.ReadLine();
+    return userPassword;
+}
+
+
+string password = "111";
+while (true)
 {
     Console.WriteLine("Введите что-нибудь:");
     string input = Console.ReadLine();
