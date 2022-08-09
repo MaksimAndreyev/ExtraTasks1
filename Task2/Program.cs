a8c5f245209feecb4fdd1d6299b223f0c7534d6b
maximandreev <maxands@yandex.ru>
2022-08-09 16:34:01 +0300

    WriteName created

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,19 @@
-﻿while (true)
+﻿void WriteName(string name, string password)
+{
+    Console.WriteLine("Для вывода имени введите пароль:");
+    string input = Console.ReadLine();
+    if (input == password)
+    {
+        Console.WriteLine(name);
+    }
+    else
+    {
+        Console.WriteLine("Неверный пароль");
+    }
+}
+
+
+while (true)
 {
     Console.WriteLine("Введите что-нибудь:");
     string input = Console.ReadLine();
