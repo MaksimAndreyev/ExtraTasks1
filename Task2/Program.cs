7a509df17c1a4a7d5b487e75c0f016207ecf022a
maximandreev <maxands@yandex.ru>
2022-08-09 16:25:50 +0300

    Help created

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,13 @@
-﻿while (true)
+﻿void Help()
+{
+    Console.WriteLine("SetName – Установить имя");
+    Console.WriteLine("SetPassword – Установить пароль");
+    Console.WriteLine("Exit – выход");
+    Console.WriteLine("WriteName – вывести имя после ввода пароля");
+}
+
+
+while (true)
 {
     Console.WriteLine("Введите что-нибудь:");
     string input = Console.ReadLine();
