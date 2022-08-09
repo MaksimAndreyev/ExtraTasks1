85023f97c6df3c5a9b2e7086057f55d38ddc7d22
maximandreev <maxands@yandex.ru>
2022-08-09 16:04:19 +0300

    Task 1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿string message = "Шкаф продан, могу предложить никелированную кровать с тумбочкой.";
+string password = "у вас продаётся славянский шкаф?";
+int attempt = 3;
+while (attempt != 0)
+{
+    Console.WriteLine("Введите пароль:");
+    string input = Console.ReadLine().ToLower();
+    if (input == password)
+    {
+        Console.WriteLine(message);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Неверно. Попробуйте ещё раз.");
+        attempt--;
+    }
+}
